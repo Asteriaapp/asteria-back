b3785d82ba5621a406cbef06df33871804a1837c
Zete0x <loic.delprat01@gmail.com>
2025-12-10 14:26:27 +0100

    feat: added api route for tables

---
+++ api/tables/models/table.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/db.config');
+
+const Class = sequelize.define('Table', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    campaign_id: {
+        type: DataTypes.UUID,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+    },
+    table_code: {
+        type: DataTypes.STRING,
+    }
+});
+
+module.exports = Class;
