c11d88f786cbefe83c9e59c02b49b8b5a3e13fc6
zoom26042604 <nathan.ferre@ynov.com>
2026-05-19 13:08:44 +0000

    feat: add Kubernetes configuration

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.ASTERIA_DB_USER,
     process.env.ASTERIA_DB_PASSWORD,
     {
-        host: 'asteria-db',
+        host: process.env.DB_HOST || 'asteria-db',
         port: 5432,
         dialect: 'postgres',
     }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:4200',
     credentials: true
 }));
 
